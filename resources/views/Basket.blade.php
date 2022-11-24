3044eabf657f420bb4f1a1aadfbc4090b06601db
Salma Tariq <210114232@aston.ac.uk>
2022-11-24 08:29:31 +0000

    "Have to restart the Basket page again"

--- resources/views/Basket.blade.php
+++ resources/views/Basket.blade.php
@@ -1,17 +1,16 @@
-<!-- <!DOCTYPE html>
-<html lang="en"> -->
+<!DOCTYPE html>
+<html lang="en">
 
 @section('title','| Basket')
 @include('layouts/head')
-@extends('layout')
 
-<!--<body>-->
+<body>
     @include('layouts/nav')
     <h1>Basket Page</h1>
     <main class="my-8">
 
 
-@section('content')
+
         <table id="cart" class="table table-hover table-condensed">
             <thead>
                 <tr>
@@ -23,27 +22,27 @@
                 </tr>
             </thead>
             <tbody>
-                @php $total = 0 @endphp
+                <?php $total = 0 ?>
                 @if(session('cart'))
-                @foreach(session('cart') as $id => $details)
-                @php $total += $details['price'] * $details['quantity'] ?>
-                <tr>
-                    <td data-th="Product">
-                        <div class="row">
-                            <div class="col-sm-3 hidden-xs"><img src="{{ $details['image'] }}" width="100" height="100" class="img-responsive" /></div>
-                            <div class="col-sm-9">
-                                <h4 class="nomargin">{{ $details['name'] }}</h4>
-                            </div>
-                        </div>
-                    </td>
-                    <td data-th="Price">${{ $details['price'] }}</td>
-                    <td data-th="Quantity">
-                        <input type="number" value="{{ $details['quantity'] }}" class="form-control quantity" />
-                    </td>
-                    <td data-th="Subtotal" class="text-center">${{ $details['price'] * $details['quantity'] }}</td>
+                    @foreach(session('cart') as $id => $details)
+                        <?php $total += $details['price'] * $details['quantity'] ?>
+                        <tr>
+                            <td data-th="Product">
+                                <div class="row">
+                                    <div class="col-sm-3 hidden-xs"><img src="{{ $details['image'] }}" width="100" height="100" class="img-responsive" /></div>
+                                    <div class="col-sm-9">
+                                        <h4 class="nomargin">{{ $details['name'] }}</h4>
+                                    </div>
+                                </div>
+                            </td>
+                            <td data-th="Price">${{ $details['price'] }}</td>
+                            <td data-th="Quantity">
+                                <input type="number" value="{{ $details['quantity'] }}" class="form-control quantity" />
+                            </td>
+                            <td data-th="Subtotal" class="text-center">${{ $details['price'] * $details['quantity'] }}</td>
 
-                    <td class="actions" data-th="">
-                        <button class="btn btn-info btn-sm update-cart" data-id="{{ $id }}"><i class="fa fa-refresh"></i></button>
+                            <td class="actions" data-th="">
+                            <button class="btn btn-info btn-sm update-cart" data-id="{{ $id }}"><i class="fa fa-refresh"></i></button>
                         <button class="btn btn-danger btn-sm remove-from-cart" data-id="{{ $id }}"><i class="fa fa-trash-o"></i></button>
                     </td>
                 </tr>
@@ -51,17 +50,20 @@
                 @endif
             </tbody>
             <tfoot>
-            <tr class="visible-xs">
-            <td class="text-center"><strong>Total {{ $total }}</strong></td>
-            </tr>
-            <tr>
-                <td><a href="{{ url('/') }}" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
-                <td colspan="2" class="hidden-xs"></td>
-                <td class="hidden-xs text-center"><strong>Total ${{ $total }}</strong></td>
-            </tr>
+                <tr>
+                    <td colspan="5" class="text-right">
+                        <h3><strong>Total ${{ $total }}</strong></h3>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5" class="text-right">
+                        <a href="{{ url('/') }}" class="btn btn-danger"> <i class="fa fa-arrow-left"></i> Continue Shopping</a>
+                        <button class="btn btn-success"><i class="fa fa-money"></i> Checkout</button>
+                    </td>
+                </tr>
             </tfoot>
         </table>
-@endsection
+
 
 
         <!-- <table>
@@ -71,12 +73,12 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach( $items as $item)
+                @foreach( $items as $items)
                 <tr>
-                    <td> {{$item->name}} </td>
-                    <td> {{$item->price }} </td>
+                    <td> {{$items->name}} </td>
+                    <td> {{$items->price }} </td>
                     <td>
-                        <div class="col-sm-3 hidden-xs"><img src="{{ $item->image }}" width="100" height="100" class="img-responsive" /></div>
+                        <div class="col-sm-3 hidden-xs"><img src="{{ $items->image }}" width="100" height="100" class="img-responsive" /></div>
                     </td>
                     
                 </tr>
@@ -93,6 +95,6 @@
 
 
     </main>
-<!-- </body>
+</body>
 
-</html> -->
+</html>
