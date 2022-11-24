0c974b9321005b827200e97a63201075f3592f61
Salma Tariq <210114232@aston.ac.uk>
2022-11-24 09:32:46 +0000

    "trying to pull the new website version from Sunny"

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Basket;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Darryldecode\Cart;
+
+class CartController extends Controller
+{
+    public function cartList()
+    {
+        $cartItems = \Cart::getContent();
+        return view('basket', ['cartItems' => $cartItems]);
+   
+    }
+
+
+    public function addToCart(Request $request)
+    {
+        \Cart::add([
+            'id' => $request->id,
+            'name' => $request->name,
+            'price' => $request->price,
+             'attributes' => array(
+                'image' => $request->image,
+             ),
+            'quantity' => 1
+
+        ]);
+        session()->flash('success', 'Product is Added to Cart Successfully !');
+        return redirect()->route('cart.list');
+    }
+
+    public function updateCart(Request $request)
+    {
+        \Cart::update(
+            $request->id,
+            [
+                'quantity' => [
+                    'relative' => false,
+                    'value' => $request->quantity
+                ],
+            ]
+        );
+
+        session()->flash('success', 'Item Cart is Updated Successfully !');
+
+        return redirect()->route('cart.list');
+    }
+
+    public function removeCart(Request $request)
+    {
+        \Cart::remove($request->id);
+        session()->flash('success', 'Item Cart Remove Successfully !');
+
+        return redirect()->route('cart.list');
+    }
+
+    public function clearAllCart()
+    {
+        \Cart::clear();
+
+        session()->flash('success', 'All Item Cart Clear Successfully !');
+
+        return redirect()->route('cart.list');
+    }
+}
