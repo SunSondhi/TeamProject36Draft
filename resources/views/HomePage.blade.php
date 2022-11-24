7a5c74bfbda4097a1e611f301f4a0916afbb61c5
SunSondhi <114810896+SunSondhi@users.noreply.github.com>
2022-11-24 09:26:00 +0000

    helped completed the basket funcionality and added new styling

--- resources/views/HomePage.blade.php
+++ resources/views/HomePage.blade.php
@@ -5,7 +5,14 @@
 @include('layouts/head')
 
 <body>
-    @include('layouts/nav')
+
+
+    <div class="top-container-homepage">
+        @include('layouts/nav')
+
+    </div>
+
+
 
     <div class="main-container">
         <div>
@@ -13,8 +20,50 @@
         </div>
 
         @if(Route::is('admin/'))
-            <h1>admin Homepage</h1>
+        <h1>admin Homepage</h1>
         @endif
+
+
+
+        <div class="main-content-homepage">
+            <div id="carouselExampleInterval" class="carousel slide" data-mdb-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-mdb-interval="1000">
+                        <img src="{{asset('images/topimg.jpg')}}" class="d-block w-100" alt="" />
+                    </div>
+                    <div class="carousel-item" data-mdb-interval="3000">
+                        <img src="{{asset('images/img1.jpg')}}" class="d-block w-100" alt="" />
+                    </div>
+                    <div class="carousel-item" data-mdb-interval="5000">
+                        <img src="{{asset('images/img2.jpg')}}" class="d-block w-100" alt="" />
+                    </div>
+                </div>
+                <button class="carousel-control-prev" data-mdb-target="#carouselExampleInterval" type="button" data-mdb-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" data-mdb-target="#carouselExampleInterval" type="button" data-mdb-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+
+
+
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="{{asset('images/img2.jpg')}}" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="{{asset('images/img1.jpg')}}" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="{{asset('images/img3.jpg')}}" alt="Third slide">
+                </div>
+            </div>
+        </div>
 
         <div class="flex-container">
             <div class="card" style="width: 18rem;">
@@ -33,6 +82,8 @@
             </div>
         </div>
     </div>
+
+
 
 
 
