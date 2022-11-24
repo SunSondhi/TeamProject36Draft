f2a1b9fc8b69f8a674bd3fa55cb305d0799fe7b0
Raheel Iqbal <210128594@aston.ac.uk>
2022-11-24 09:22:55 +0000

    I have completed the about us page and added text to it

--- resources/views/Aboutus.blade.php
+++ resources/views/Aboutus.blade.php
@@ -7,10 +7,35 @@
 <body>
     @include('layouts/nav')
 
-    <h1>About Us page</h1>
+    <h1>About Us</h1>
 
-    <h1>Results</h1>
+    <p1>We are Pegasus jewllers, we're a team of 7 jewllers teamed up to fill the void of good bespoke jewllery in todays market. 
+        With a beautiful range of stock and a great team we've been selling bespoke jewels and other peices to many people since 
+    we were established in 2022. We have 7 experts with different ideas and opinions we can assure that when shopping with pegasus jewllery
+    you will leave satisfied with no idea left unattended to. Our aim is to show that pegasus jewllery is the only jewllers that anyone needs.
 
+ </p1>
+<br> </br>
+ <p2> Pegasus was opened due to a lack of satisfaction from jewllery buyers up and down the country, we had seen that clients and customers
+    were not well done by and we felt that are team of experts could cater massivley to this declining industry and fill the hole being left by many
+    sellers. We have a team of expert craftsman who utilise cutting technology to create top quality and durable pieces of jewllery
+     ready to find a peice of jewllery right for you and your needs. 
+ </p2>
+ <br></br>
+
+
+
+<p3> We make and sell jewllery for any occasion, whether its that time where you need to buy a ring for that special someone, or just a loving gift
+    to someone or even a treat for yourself pegasus jewllery is here to help and will help. At pegasus we pride ourslef on proffesisonalism and so we put our full efforts
+    into making our customers happy, this is why we can assure you that you will never leave dissapointed.
+</p3>
+
+
+
+
+
+
+@include('layouts/footer')
 
 </body>
 
