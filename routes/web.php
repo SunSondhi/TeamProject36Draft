f75e2de3c28f48063f6c7ca78d141dcd064ef0cc
Salma Tariq <210114232@aston.ac.uk>
2022-11-26 17:58:04 +0000

    updating products page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Middleware\AdminMiddleWare;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ProductController;
-
+use App\Http\Controllers\CartController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,20 +26,20 @@ Route::get('products', [App\Http\Controllers\Controller::class, 'listProducts'] 
     return view("Products");
 })->name('Products');
 
-Route::get('basket', [ProductController::class, 'viewAll'],function () {
+Route::get('basket', [CartController::class, 'cartList'],function () {
     return view("Basket");
 })->name('Basket');
 
 
-Route::get('HomePage', [ProductController::class, 'index']);
-Route::get('Basket', [ProductController::class, 'cart'])->name('cart');
-Route::get('add-to-cart/{id}', [ProductController::class, 'addToCart'])->name('add-to-cart');
-Route::patch('update-cart/{request}', [ProductController::class, 'update'])->name('update-cart');
-Route::delete('remove-from-cart/{request}', [ProductController::class, 'remove'])->name('remove-from-cart');
+Route::get('HomePage', [ProductController::class, 'productList'])->name('products.list');;
+Route::get('cart', [CartController::class, 'cartList'])->name('cart.list');
+Route::post('cart', [CartController::class, 'addToCart'])->name('cart.store');
+Route::post('update-cart', [CartController::class, 'updateCart'])->name('cart.update');
+Route::post('remove', [CartController::class, 'removeCart'])->name('cart.remove');
+Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
 
 
-
-Route::get('aboutus', [App\Http\Controllers\Controller::class, 'search'], function () {
+Route::get('aboutus', function () {
     return view("Aboutus");
 })->name('Aboutus');
 
@@ -64,13 +64,15 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
 Route::group([
     'namespace' => 'admin',
     'prefix' => 'admin',
     'middleware' => 'auth',
     'middleware' => 'isAdmin',
 ], function () {
-    Route::get('adminhome', [App\Http\Controllers\HomeController::class, 'admin_home'])->name('admin.adminhome')->middleware('isAdmin');
+    Route::get('adminhome',[App\Http\Controllers\Controller::class, 'search'],[App\Http\Controllers\HomeController::class, 'admin_home'])->name('admin.adminhome')->middleware('isAdmin');
+
 
     Route::get('/', function () {
         return view("HomePage");
@@ -84,7 +86,7 @@ Route::group([
         return view("Basket");
     })->name('admin.Basket');
 
-    Route::get('/aboutus', [App\Http\Controllers\Controller::class, 'search'],function () {
+    Route::get('/aboutus',function () {
         return view("Aboutus");
     })->name('admin.Aboutus');
 
